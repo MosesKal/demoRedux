3bb7529056bcb07455ebdbaa9dc91bbc2c091f29
MosesK <mosesziongo@gmail.com>
2023-04-05 10:29:39 +0200

    fonctionnalite affichage du produit, routage lorsqu'on click sur un produit déjà fonctionnel

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import { productReducer, selectedProductsReducer } from "./productsReducer";
+
+const reducers = combineReducers({
+  allProducts: productReducer,
+  product: selectedProductsReducer,
+});
+export default reducers;
